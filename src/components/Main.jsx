95f673e4a946cce2b131af0263b567f64e8a48e7
Ayman Elh <elhamioui1964@gmail.com>
2025-06-29 13:47:29 +0100

    feat: implement the form component:

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main() {
+    return (
+        <main>
+            <form className="add-ingredient-form">
+                <input className="" type="text" aria-label="Add Ingredient" placeholder="e.g. oregano"/>
+                <button>Add Ingredient</button>
+            </form>
+        </main>
+    )
+}
