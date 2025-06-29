a2bb9962c992f93bd515b08ba874339900287cf9
Ayman Elh <elhamioui1964@gmail.com>
2025-06-29 17:37:18 +0100

    feat: mapping the ingredients and print them on page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,27 @@
+import {useState} from "react";
+
 export default function Main() {
+
+    const [ingredients, setIngredients] = useState([]);
+
+    function handleSubmitForm(e) {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const newIngredient = formData.get("ingredient")
+        setIngredients([...ingredients, newIngredient])
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
-                <input className="" type="text" aria-label="Add Ingredient" placeholder="e.g. oregano"/>
+            <form className="add-ingredient-form" onSubmit={handleSubmitForm}>
+                <input type="text" name="ingredient" aria-label="Add Ingredient" placeholder="e.g. oregano"/>
                 <button>Add Ingredient</button>
             </form>
+            <ul>
+                {ingredients && ingredients.map(function(ingredient) {
+                    return <li key={ingredient}>{ingredient}</li>
+                })}
+            </ul>
         </main>
     )
 }
